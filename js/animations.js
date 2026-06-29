76c278c86ab47577dcc65158d3d9317ef31c2d8a
avedesign <mail@ave-design.com>
2026-06-29 12:23:19 +0200

    2

--- js/animations.js
+++ js/animations.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
       ease: "none",
       scrollTrigger: {
         trigger: element,
-        start: "top 130%",
+        start: "top 100%",
         end: "top 20%",
         scrub: 1.2
       }
